a26d247d6dd32a04c5711e648f59b011d1ee2562
Jukko96 <cslzh4433@gmail.com>
2026-05-02 10:38:19 +0900

    fix setting, test code1

---
+++ src/test/java/hello/hello_spring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,23 @@
+package hello.hello_spring.repository;
+
+import hello.hello_spring.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MemoryMemberRepositoryTest {
+    MemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+        System.out.println("result = " + (result == member));
+//        Assertions.assertEquals(result,member);
+        Assertions.assertThat(member).isEqualTo(result);
+    }
+
+}
